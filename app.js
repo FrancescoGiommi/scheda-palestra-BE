22605298f8055614e7578ee863258f8fc4727a23
Francesco <francescogiommi97@icloud.com>
2025-10-05 19:43:35 +0200

    Add setup package.json and file env

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+const { APP_HOST, APP_PORT } = process.env;
+
+app.listen(APP_PORT, () => {
+  console.log(`Server listening at ${APP_HOST}:${APP_PORT}`);
+});
